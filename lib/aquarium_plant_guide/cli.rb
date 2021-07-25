329b8567e91251a50d0283c49f38b44781dcb941
Kristyn <kristynlittle@frontier.com>
2021-07-25 16:41:50 -0700

    added files to bin and lib

---
+++ lib/aquarium_plant_guide/cli.rb
@@ -0,0 +1,15 @@
+class AquariumPlantGuide::CLI
+    
+    def start
+        puts "welcome to the Aquarium Plant Guide"
+        #list_plants
+    end
+
+    #def list_plants
+
+    #end
+
+    #def list_options
+    #    puts
+
+end
