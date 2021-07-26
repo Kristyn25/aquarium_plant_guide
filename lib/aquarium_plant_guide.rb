e86fc823e1aab6ae7fdebff585b216edc799e725
Kristyn <kristynlittle@frontier.com>
2021-07-25 17:26:16 -0700

    Saving files before refreshing line endings

--- lib/aquarium_plant_guide.rb
+++ lib/aquarium_plant_guide.rb
@@ -1,8 +1,4 @@
 # frozen_string_literal: true
 
-require_relative "aquarium_plant_guide/version"
-
-module AquariumPlantGuide
-  class Error < StandardError; end
-  # Your code goes here...
-end
+require_relative "./aquarium_plant_guide/version"
+require_relative './aquarium_plant_guide/cli'
