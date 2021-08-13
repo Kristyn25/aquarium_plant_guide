d93a16e9ddfc51058c1340c29d11ab2d873cc6f6
Kristyn <kristynlittle@frontier.com>
2021-08-12 18:08:21 -0700

    list_options menu

--- lib/aquarium_plant_guide.rb
+++ lib/aquarium_plant_guide.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
 require_relative "./aquarium_plant_guide/version"
-require_relative './aquarium_plant_guide/cli'
+require_relative "./aquarium_plant_guide/cli"
--- lib/aquarium_plant_guide/cli.rb
+++ lib/aquarium_plant_guide/cli.rb
@@ -2,14 +2,25 @@ class AquariumPlantGuide::CLI
     
     def start
         puts "welcome to the Aquarium Plant Guide"
-        #list_plants
+        list_plants
+        list_options
     end
 
-    #def list_plants
+    def list_plants
+        puts "1. Red Dwarf Aquarium Lily"
+        puts "2. Anubias nana petite"
+    end
 
-    #end
-
-    #def list_options
-    #    puts
-
+    def list_options
+    puts "Enter the number of the plant you would like more info on or type exit"
+        if input != 0
+            input = gets.strip
+        elsif "1"
+            puts "More info on Red Dwarf Aquarium Lily..."
+        elsif "2"
+            puts "More info on Anubias nana petite..."
+        else
+            puts "I am not sure what you are looking for."
+        end
+    end
 end
